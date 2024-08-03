541ce62aa31a7624082f611ae1b80a2b2b8fab2e
Sebastian Galarza <95152236+sebferreira@users.noreply.github.com>
2024-08-03 15:55:44 -0300

    Update clientShared.js

--- front-end/src/shared/clientShared.js
+++ front-end/src/shared/clientShared.js
@@ -1,14 +1,27 @@
-const handleCustomApiRequest = async ({url, method, body}) => {
+import Cookies from "js-cookie";
+
+const handleCustomApiRequest = async ({url, method, body, withToken}) => {
+  const cookie = Cookies.get();
+  let headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "https://poloweb.vercel.app",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    "Access-Control-Request-Headers": "Content-Type",
+    "Access-Control-Request-Method": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+  };
+  if (withToken) {
+    const Authorization = `Bearer ${cookie.token}`;
+    headers = {...headers, Authorization};
+  }
   const fetching = await fetch(url, {
-    method,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "http://localhost:5173",
-    },
-    credentials: "include",
-    body: body ? JSON.stringify(body) : undefined,
-  });
-  const petition = await fetching.json();
+      method,
+      headers,
+      credentials: "include",
+      body: body ? JSON.stringify(body) : undefined,
+    }),
+    petition = await fetching.json();
   return petition;
 };
 
