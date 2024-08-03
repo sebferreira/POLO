c1f0e587ae0d63189571e0d20a760fcca4fa2c2c
Sebastian Galarza <95152236+sebferreira@users.noreply.github.com>
2024-08-03 16:00:33 -0300

    vercel

--- back-end/src/middlewares/authorization.middleware.js
+++ back-end/src/middlewares/authorization.middleware.js
@@ -6,7 +6,7 @@ config();
 
 export async function revisarCookie(req, res, next) {
   try {
-    const token = req.cookies.token;
+      const token = req.headers.authorization.split(" ")[1];
     console.log(token);
     if (!token) return res.status(401).json(["Unauthorized"]);
     jwt.verify(token, process.env.SECRET_KEY, async (err, decoded) => {
