33740da33f7ccb585139344cbd0b3374c36d4a88
Sebastian Galarza <95152236+sebferreira@users.noreply.github.com>
2024-09-08 19:41:17 -0300

    Update index.jsx

--- front-end/src/pages/Tables/index.jsx
+++ front-end/src/pages/Tables/index.jsx
@@ -6,7 +6,7 @@ import {Link} from "react-router-dom";
 
 export default function Tables() {
   const {boards} = useBoard();
-  console.log(boards)
+ 
   return (
     <>
       <Box sx={{display: {sx: "flex", md: "none"}}}>
