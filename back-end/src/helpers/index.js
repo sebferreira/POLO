f1e6c10c9ab0048b883683c9e3b488755b0a3720
sebferreira <sebastianleonelgalarza@gmail.com>
2024-10-06 14:21:01 -0300

    arreglo de middleware imagenes

--- back-end/src/helpers/index.js
+++ back-end/src/helpers/index.js
@@ -1,9 +1,6 @@
 // Importa el modelo Board para realizar operaciones en la base de datos de boards
 import Board from "../models/boards.model.js";
-import * as fs from "node:fs";
-import multer from "multer";
-import path from "path";
-const __dirname = path.resolve();
+
 // Función para actualizar la fecha de un board específico
 export async function setUpdateDateFromBoard({boardId}) {
   // Actualiza la columna 'date' de un board específico con la fecha y hora actual
@@ -12,22 +9,3 @@ export async function setUpdateDateFromBoard({boardId}) {
     {where: {id_board: boardId}} // Filtro para encontrar el board por su ID
   );
 }
-const diskstorage = multer.diskStorage({
-  destination: path.join(__dirname, "src/uploads/"),
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-
-export const fileUpload = multer({
-  storage: diskstorage,
-}).single("TaskImage");
-
-/* 
-export async function saveImage(file) {
-  const format = file.originalname.split(".")[1];
-  const newPath = `./src/uploads/${file.filename}.${format}`;
-  fs.renameSync(file.path, newPath);
-
-  return `${file.filename}.${format}`;
-} */
--- back-end/src/routes/tasks.routes.js
+++ back-end/src/routes/tasks.routes.js
@@ -12,7 +12,7 @@ import {
   insertImage,
   updateTask,
 } from "../controllers/task.controller.js"; // Importa los controladores de tarea
-import {fileUpload} from "../helpers/index.js";
+import {fileUpload} from "../middlewares/imageUploads.middleware.js";
 const routerTask = express.Router(); // Crea un enrutador para manejar las rutas relacionadas con tareas
 
 // Define las rutas para las operaciones de tareas
